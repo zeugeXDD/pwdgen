9bbfae298528dee2c2d3e11c2471e32b42fecb5c
zeugeXDD <52102914+zeugeXDD@users.noreply.github.com>
2019-06-22 15:15:03 +0300

    Add files via upload
    password generator

---
+++ password generator.py
@@ -0,0 +1,10 @@
+import random
+import string
+chars = string.ascii_letters + string.digits + '!@#$%^&*'
+
+length = input('password length?: ')
+length = int(length)
+password = ''
+for c in range(length):
+    password += random.choice(chars)
+print(password)
