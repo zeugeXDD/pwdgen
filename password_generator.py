9e107b4bbd557556aa154015486e459cefab4b3e
zeugeXDD <52102914+zeugeXDD@users.noreply.github.com>
2019-07-10 20:27:47 +0000

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -1,10 +1,29 @@
 import random
 import string
-chars = string.ascii_letters + string.digits + '!@#$%^&*'
+
+print('''
+    
+ /$$$$$$$$                                        
+|_____ $$                                         
+     /$$/   /$$$$$$  /$$   /$$  /$$$$$$   /$$$$$$ 
+    /$$/   /$$__  $$| $$  | $$ /$$__  $$ /$$__  $$
+   /$$/   | $$$$$$$$| $$  | $$| $$  \ $$| $$$$$$$$
+  /$$/    | $$_____/| $$  | $$| $$  | $$| $$_____/
+ /$$$$$$$$|  $$$$$$$|  $$$$$$/|  $$$$$$$|  $$$$$$$
+|________/ \_______/ \______/  \____  $$ \_______/
+                               /$$  \ $$          
+                              |  $$$$$$/          
+                               \______/    
+    ''')
+chars = string.ascii_letters + string.digits + '!@#$%^&*()_+-=`]}['
 
 length = input('password length?: ')
 length = int(length)
+
 password = ''
+
 for c in range(length):
+
     password += random.choice(chars)
+
 print(password + ' is your password.')
